7e1638341f9ed1c2e322c6358e777d78a6265d87
Nomade <102302877+TheArabicMonster@users.noreply.github.com>
2024-11-16 09:17:02 +0100

    feat(forms) backround color
    45min-DONE

--- Code/BitRuisseau/Form1.Designer.cs
+++ Code/BitRuisseau/Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            label1 = new Label();
+            panel3 = new Panel();
+            label2 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 138);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(158, 313);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(panel3);
+            panel1.Location = new Point(0, -2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(158, 44);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(20, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(118, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Liste de fichiers";
+            label1.Click += label1_Click;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(-21, -17);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(891, 504);
+            panel3.TabIndex = 7;
+            panel3.Paint += panel3_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(5, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 16);
+            label2.TabIndex = 2;
+            label2.Text = "Partage commun";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(46, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(68, 16);
+            label4.TabIndex = 4;
+            label4.Text = "Personnel";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(134, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Location = new Point(1, 93);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(179, 358);
+            panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Paint += Form1_Paint;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private TextBox textBox1;
+        private Panel panel2;
+        private Panel panel3;
     }
 }
--- Code/BitRuisseau/Form1.cs
+++ Code/BitRuisseau/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+
 namespace BitRuisseau
 {
     public partial class Form1 : Form
@@ -5,6 +7,69 @@ namespace BitRuisseau
         public Form1()
         {
             InitializeComponent();
+            //SetupPanel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            Panel panel = sender as Panel;
+
+            Color colorLeft = Color.Blue;
+            Color colorRight = Color.LightBlue;
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                panel.ClientRectangle, colorLeft, colorRight, LinearGradientMode.Horizontal))
+            {
+                e.Graphics.FillRectangle(brush, panel.ClientRectangle);
+            }
+        }
+        private void SetupPanel()
+        {
+            Panel panel = new Panel
+            {
+                Location = new System.Drawing.Point(20, 20),
+                Size = new System.Drawing.Size(400, 200)
+            };
+            panel.Paint += panel3_Paint;
+            this.Controls.Add(panel);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Color colorLeft = Color.Blue;
+            Color colorRight = Color.LightBlue;
+
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle, colorLeft, colorRight, LinearGradientMode.Horizontal))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
         }
     }
 }
